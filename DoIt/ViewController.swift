011f999d6e8979cd2549b90b543a9c7e38472f37
JT9 <CT@Cs-iMac.local>
2017-10-03 18:11:28 -0700

    Created an Initial TableView to setup
    A basic TableView setup with just 10 rows and a label to test first

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//Need to add UITableViewDelegate & DataSource in order to use TableView
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Needs these two for Tableview to look to this class
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //How many rows
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //New UITableView object
+        let cell = UITableViewCell()
+        //Label for tableView
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
