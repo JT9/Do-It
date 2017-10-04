1f989c9e271c7142c314e8ac739959853ea3ac0d
JT9 <CT@Cs-iMac.local>
2017-10-03 22:34:42 -0700

    Added Core Data to the App and removed code that was used to create the data before
    Added the user of Core Data to the app but the app crashs when you add a task as the rest of Core Data still needs to be implemented  Also removed code that was used to create tasks because the app wil be using Core Data instead

--- DoIt/TasksViewController.swift
+++ DoIt/TasksViewController.swift
@@ -23,9 +23,6 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Using func to make tasks from Task class
-        tasks = makeTasks()
-        
         //Needs these two for Tableview to look to this class
         tableView.dataSource = self
         tableView.delegate = self
@@ -69,30 +66,6 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     
-    
-    
-    //Used to make task objects
-    func makeTasks() -> [Task] {
-        
-        //Used to test the tableView with object data
-        let task1 = Task()
-        task1.name = "Walk the dog"
-        task1.important = false
-        
-        let task2 = Task()
-        task2.name = "Buy Cheese"
-        task2.important = true
-        
-        let task3 = Task()
-        task3.name = "Mow the lawn"
-        task3.important = false
-        
-        //returning 3 tester tasks
-        return [task1, task2, task3]
-        
-        
-        
-    }
     @IBAction func plusTapped(_ sender: Any) {
         //Used to move to another ViewController from pushing the plus button in the Nav Bar
         performSegue(withIdentifier: "addSegue", sender: nil)
