fa400a9531a87a72c22d34de0c524f56daecd31e
JT9 <CT@Cs-iMac.local>
2017-10-03 21:07:57 -0700

    Created a new Cocoa Touch Class, and linked outlets and action
    Added a new Cocoa Touch Class called CreateTaskViewController.  Also linked the TextField, Switch and Button to this ViewController

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by C T on 10/3/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
